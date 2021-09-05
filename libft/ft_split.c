3c73262f5783ddc27d8216c38a2e518ffbc9fb17
effervescinn <32834764+effervescinn@users.noreply.github.com>
2021-09-05 19:35:34 +0300

    ???

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -12,77 +12,84 @@
 
 #include "libft.h"
 
-static	int	 quantity(const char *str, char c)
+static int	count_words(char const *s, char c)
 {
-	int	q;
+	int	w_q;
 
-	q = 0;
- 	while (*str != '\0')
+	w_q = 0;
+	while (*s)
 	{
-		while (*str == c)
+		while (*s == c)
 		{
-			str++;
-			if (*str == '\0')
-			return(q);
+			s++;
+			if (*s == '\0')
+				return (w_q);
 		}
-		if (*str != c)
-		q++;
- 		while (*str != c)
+		while (*s != c)
 		{
-			str++;
-			if (*str == '\0')
-			return (q);
+			s++;
+			if (*s == '\0')
+				return (++w_q);
 		}
-	}
-	return (q);
-}
-
-static	int	ft_strlensep (const char *s, char c)
-{
-	int	len;
-
-	len = 0;
-	while (*s != c && *s != '\0')
-	{
+		w_q++;
 		s++;
-		len++;
 	}
-	return (len);
+	return (w_q);
 }
 
-static	void	freee(char **arr, int k)
+static int	free_all(char **arr, int i, int w_len)
 {
-	while (k--)
-		free(arr[k]);
-	free(arr);
+	if (!(arr[i] = (char *)malloc(w_len * sizeof(char) + sizeof(char))))
+	{
+		while (i-- >= 0)
+			free(arr[i]);
+		free(arr);
+		return (1);
+	}
+	return (0);
+}
+
+static int	push_strs(const char *s, char c, char **arr, int w_q)
+{
+	char	*w_start;
+	int		w_len;
+	int		i;
+
+	i = 0;
+	while (*s && i < w_q)
+	{
+		w_len = 0;
+		while (*s == c && *s)
+			s++;
+		if (*s)
+			w_start = (char *)s;
+		while (*s != c && *s)
+		{
+			w_len++;
+			s++;
+		}
+		if ((free_all(arr, i, w_len) == 1))
+			return (1);
+		ft_strlcpy(arr[i], w_start, w_len + 1);
+		i++;
+	}
+	arr[i] = NULL;
+	return (0);
 }
 
 char		**ft_split(char const *s, char c)
 {
 	char	**arr;
-	size_t	j;
-	int	k;
-	int	i;
+	int		w_q;
 
-	i = 0;
-	if (!s || !((arr = (char **)malloc((quantity(s, c) + 1) * sizeof(char *)))))
+	if (s == NULL)
 		return (NULL);
-	k = 0;
-	while (k != quantity(s, c))
+	w_q = count_words(s, c);
+	if ((arr = (char **)malloc(w_q * sizeof(char *) + sizeof(char *))))
 	{
-		while (s[i] == c && s[i] != '\0')
-			i++;
-		if (!(arr[k] = (char*)malloc(sizeof(char) * (ft_strlensep(&s[i], c) + 1))))
-		{
-			freee(arr, k);
+		if ((push_strs(s, c, arr, w_q) == 1))
 			return (NULL);
-		}
-		arr[k][ft_strlensep(&s[i], c)] = '\0';
-		j = 0;
-		while (s[i] != c && s[i] != '\0')
-			arr[k][j++] = s[i++];
-		k++;
+		return (arr);
 	}
-	arr[k] = NULL;
-	return (arr);
+	return (NULL);
 }
